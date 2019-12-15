938a6a7456a67a9ed19295e1220a321535f27629
Justin Chin <justinmichaelchin@gmail.com>
2019-12-14 19:15:59 -0800

    cleans up old comments

--- client.py
+++ client.py
@@ -69,8 +69,6 @@ if __name__ == "__main__":
     parser.add_argument("hostname", nargs='?',
                         default="http://127.0.0.1:5000",
                         help="{protocol}://{address}:{port}")       # host to connect
-    # parser.add_argument("-g", "--get", choices=['','',''])
-    # parser.add_argument("p", "--put", nargs=3)  # {user} {topic} "{msg}"
     parser.add_argument("operation", choices=['put','get'], help="One of 'get' or 'put'")
     parser.add_argument("-u", "--user")     # user
     parser.add_argument("-t", "--topic")    # topic/channel
@@ -85,37 +83,7 @@ if __name__ == "__main__":
     elif args.operation == "put":
         # put some shit
         user, topic, msg = args.user, args.topic, args.message
-
         if user and topic and msg:
             put(args.hostname, user, topic, msg)
-
-    # TODO: I've parsed the arguments and have stored them in a
-    # temporary variable, now I need to compose them together to
-    # create the pattern for matching a tuplespace TAKE if the action
-    # is "get" or a message representing the tuplespace WRITE if the
-    # action is "put"
-
-    # if sys.argv[2] == "get":
-    #     addr = sys.argv[1]
-    #     tupl = tuple(sys.argv[3:])
-    #     print(addr)
-    #     print(tupl)
-    #     get(addr, tupl)
-    # # if len(sys.argv) == 3:
-    # #     # addr, key
-    # #     # get
-    # #     addr = sys.argv[1]
-    # #     tupl = sys.argv[2]
-    # #     get(addr, tupl)
-    # elif len(sys.argv) == 5:
-    #     # addr, key value
-    #     # put
-    #     addr = sys.argv[1]
-    #     user = sys.argv[2]
-    #     topic = sys.argv[3]
-    #     channel = sys.argv[4]
-    #     put(addr, user, topic, channel)
-    # else:
-    #     print("PUT usage: python3 client.py address 'key' 'value'")
-    #     print("GET usage: python3 client.py address 'key'")
-    #     print("Format: address: http://ip:port")
+        else:
+            print(f'Error: PUT must have "user", "topic" and "msg"')
