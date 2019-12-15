f9af50a4b3c51ee988e8078905d5f49e1ff36468
Justin Chin <justinmichaelchin@gmail.com>
2019-12-14 16:19:42 -0800

    adds an argument parser to the server
    by adding an argument parser, we are able to easily allow the user to
    distinguish various parts of their message (i.e "user",
    "topic/channel", "message"). They can give us all, or none of the
    aforementioned message parts which we can use to dynamically construct
    a tuple pattern to match on for GET requests. PUT requests remain
    unchanged, and should have a user, topic/channel and message to be a
    complete, well-formatted request.

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import argparse
 import sys, requests
+import argparse
 
 
 def redirectToLeader(server_address, message):
@@ -52,6 +53,8 @@ def put(addr, user, topic, content):
 #     # redirecting till we find the leader, in case of request during election
 #     print(redirectToLeader(server_address, message))
 
+# Proxy is expecting python types when mapping templates out for
+# Py=>Rb conversion
 def get(addr, tupl):
     server_address = addr + "/request"
     payload = {"pattern": tupl}
@@ -60,27 +63,57 @@ def get(addr, tupl):
 
 
 if __name__ == "__main__":
-    if sys.argv[2] == "get":
-        addr = sys.argv[1]
-        tupl = tuple(sys.argv[3:])
-        print(addr)
-        print(tupl)
-        get(addr, tupl)
-    # if len(sys.argv) == 3:
-    #     # addr, key
-    #     # get
+    # python client.py 127.0.0.1:5000 get -user alice -topic distsys
+    # => [("alice", "distsys", "foo"), ("alice", "distsys", "bar")...]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hostname", nargs='?',
+                        default="http://127.0.0.1:5000",
+                        help="{protocol}://{address}:{port}")       # host to connect
+    # parser.add_argument("-g", "--get", choices=['','',''])
+    # parser.add_argument("p", "--put", nargs=3)  # {user} {topic} "{msg}"
+    parser.add_argument("operation", choices=['put','get'], help="One of 'get' or 'put'")
+    parser.add_argument("-u", "--user")     # user
+    parser.add_argument("-t", "--topic")    # topic/channel
+    parser.add_argument("-m", "--message")
+    args = parser.parse_args()
+
+    if args.operation == "get":
+        # get some shit
+        print(f"GET ({args.user} {args.topic} {args.message})")
+    elif args.operation == "put":
+        # put some shit
+        user, topic, msg = args.user, args.topic, args.message
+
+        if user and topic and msg:
+            put(args.hostname, user, topic, msg)
+
+    # TODO: I've parsed the arguments and have stored them in a
+    # temporary variable, now I need to compose them together to
+    # create the pattern for matching a tuplespace TAKE if the action
+    # is "get" or a message representing the tuplespace WRITE if the
+    # action is "put"
+
+    # if sys.argv[2] == "get":
     #     addr = sys.argv[1]
-    #     tupl = sys.argv[2]
+    #     tupl = tuple(sys.argv[3:])
+    #     print(addr)
+    #     print(tupl)
     #     get(addr, tupl)
-    elif len(sys.argv) == 5:
-        # addr, key value
-        # put
-        addr = sys.argv[1]
-        user = sys.argv[2]
-        topic = sys.argv[3]
-        channel = sys.argv[4]
-        put(addr, user, topic, channel)
-    else:
-        print("PUT usage: python3 client.py address 'key' 'value'")
-        print("GET usage: python3 client.py address 'key'")
-        print("Format: address: http://ip:port")
+    # # if len(sys.argv) == 3:
+    # #     # addr, key
+    # #     # get
+    # #     addr = sys.argv[1]
+    # #     tupl = sys.argv[2]
+    # #     get(addr, tupl)
+    # elif len(sys.argv) == 5:
+    #     # addr, key value
+    #     # put
+    #     addr = sys.argv[1]
+    #     user = sys.argv[2]
+    #     topic = sys.argv[3]
+    #     channel = sys.argv[4]
+    #     put(addr, user, topic, channel)
+    # else:
+    #     print("PUT usage: python3 client.py address 'key' 'value'")
+    #     print("GET usage: python3 client.py address 'key'")
+    #     print("Format: address: http://ip:port")
